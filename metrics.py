cf286df9c7854abaea5156e5ef7aa63e886c05b1
TxCorpi0x <6095314+TxCorpi0x@users.noreply.github.com>
2024-01-05 18:49:12 +0300

    feat: notebook content

--- metrics.py
+++ metrics.py
@@ -24,27 +24,19 @@ def mae_auc_score(model, ratings):
     auc : float 0.0 ~ 1.0
     """
 
-    # y_actual  = []
-    # y_prediction  = []
     auc = 0.0
-    rms = 0.0
-    rmse = 0.0
     n_users, n_items = ratings.shape
+    y_trues = []
+    y_preds = []
     for user, row in enumerate(ratings):
         y_pred = model._predict_user(user)
-        # y_prediction.append(y_pred)
+        y_preds.append(y_pred)
         y_true = np.zeros(n_items)
-        # y_actual= y_true[row.indices]
         y_true[row.indices] = 1
+        y_trues.append(y_true)
 
         auc += roc_auc_score(y_true, y_pred)
-        # x= mean_squared_error(y_true, y_pred, squared=False)
-        # if x >1:
-        #     print(x)
-        rms += mean_squared_error(y_true, y_pred, squared=False)
 
     auc /= n_users
-    rms /= n_users
 
-    # rms = mean_squared_error(y_actual, y_prediction, squared=False)
-    return rms, auc
+    return auc, y_trues, y_preds
